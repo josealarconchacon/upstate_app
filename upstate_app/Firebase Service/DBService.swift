38715ee337fd6eb7e82fcfbbda8a82c00bcb2a96
Jose Carlos <josealarconchacon@gmail.com>
2020-06-25 20:03:14 -0400

    Firebase Service

--- upstate_app/Firebase Service/DBService.swift
+++ upstate_app/Firebase Service/DBService.swift
@@ -1,9 +1,25 @@
+////
+////  DBService.swift
+////  upstate_app
+////
+////  Created by Jose Alarcon Chacon on 6/25/20.
+////  Copyright © 2020 Jose Alarcon Chacon. All rights reserved.
+////
 //
-//  DBService.swift
-//  upstate_app
+//import Foundation
 //
-//  Created by Jose Alarcon Chacon on 6/25/20.
-//  Copyright © 2020 Jose Alarcon Chacon. All rights reserved.
 //
-
-import Foundation
+//final class DBService {
+//    private init() {}
+//    public static var firestore: Firestore = {
+//        let dataBase = Firestore.firestore()
+//        let setting = dataBase.settings
+//        setting.areTimestampsInSnapshotsEnabled = true
+//        dataBase.settings = setting
+//        return dataBase
+//    }()
+//            
+////    static public var generateDocumentId: String {
+////        return firestore.collection(UserKey.UserCollectionKey).document().documentID
+////    }
+//}
