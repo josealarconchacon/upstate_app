784fe237c0c614edd300eaa7f448b3690cfd8574
Jose Carlos <josealarconchacon@gmail.com>
2020-06-29 19:08:56 -0400

    add Town Model

--- upstate_app/Model/Town.swift
+++ upstate_app/Model/Town.swift
@@ -6,4 +6,41 @@
 //  Copyright © 2020 Jose Alarcon Chacon. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import CoreLocation
+
+class Town {
+    let townID: String
+    let townName: String
+    let townImage: String?
+    let description: String
+    let latitude: Double
+    let longitude: Double
+    let categories: [String]
+    let transit: [String]
+    let type: [String]
+    
+    init(townID: String, townName: String, townImage: String, description: String, latitude:Double, longitude: Double, categories: [String], transit: [String], type: [String]) {
+        self.townID = townID
+        self.townName = townName
+        self.townImage = townImage
+        self.description = description
+        self.latitude = latitude
+        self.longitude = longitude
+        self.categories = categories
+        self.transit = transit
+        self.type = type
+    }
+    
+    init(dict: [String: Any]) {
+        self.townID      = dict[TownCollectionKey.TownIDKey] as? String ?? ""
+        self.townName    = dict[TownCollectionKey.NameKey] as? String ?? ""
+        self.townImage   = dict[TownCollectionKey.ImageKey] as? String ?? ""
+        self.description = dict[TownCollectionKey.DescriptionKey] as? String ?? ""
+        self.latitude    = dict[TownCollectionKey.LatitudeKey] as? Double ?? 0.0
+        self.longitude   = dict[TownCollectionKey.LongitudeKey] as? Double ?? 0.0
+        self.categories  = dict[TownCollectionKey.CategoriesKey] as? [String] ?? [""]
+        self.transit     = dict[TownCollectionKey.TransitKey] as? [String] ?? [""]
+        self.type        = dict[TownCollectionKey.TypeKey] as? [String] ?? [""]
+    }
+}
